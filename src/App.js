5b212f9f732882086f5e760bae60a7c2799bb024
unknown <andreabcm16@gmail.com>
2022-01-12 18:58:44 +0100

    using variables and data collections in App.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,27 @@
+import React, { Component } from "react";
 import Header from "./components/header";
 import Dish, { Flag } from "./components/dish";
 
 import './styles/App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <Header/>
-      <Dish/>
-      <Flag/>
-    </div>
-  );
+class App extends Component {
+  dish = "tortilla";
+  dishes = ["Tortilla", "Paella", "Xató"];
+
+  render() {
+    return (
+      <div className="App">
+        <Header/>
+        <Dish/>
+        Me gusta la { this.dish }
+        <ul>
+          {this.dishes.map(dish => (
+            <li>{dish}</li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
 }
 
 export default App;
