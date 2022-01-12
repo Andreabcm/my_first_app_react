ca8af8ae28cca859af808708f8cceff66ed6f3d8
unknown <andreabcm16@gmail.com>
2022-01-12 12:13:12 +0100

    creating the dish component

---
+++ src/components/dish.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Dish extends Component{
+    render(){
+        return(
+            <div>
+                <h1>Platos</h1>
+            </div>
+        )
+    }
+}
+
+export default Dish;
